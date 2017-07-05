e5e1cc1c97a65eb5460527ea19db115e63e0944c
Shawn Carnegie <carnegieshawn@yahoo.ca>
2017-07-04 19:24:49 -0600

    Add root route with links to sign up, sign in, and sign out
    Also:
     - Added Warden test helpers for login_as
     - Created factory for confirmed_user
     - Added visit_root spec to test for visible authentication links
     - Added additonal user_spec tests to test confirmed attribute on user models
    
    resolves: #8, #32

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,17 @@
 require 'rails_helper'
  
 RSpec.describe User, type: :model do
-  it 'has a valid factory' do
+  it 'has valid factories' do
     expect(FactoryGirl.build(:user)).to be_valid
+  end
+
+  describe '#confirmed' do
+    it 'is true in a confirmed user' do
+      expect(FactoryGirl.build(:user, :confirmed).confirmed?).to be true
+    end
+
+    it 'is false in an unconfirmed user' do
+      expect(FactoryGirl.build(:user).confirmed?).to be false
+    end
   end
 end
