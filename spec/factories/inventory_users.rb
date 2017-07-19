27c6a0132d677a287811d93bacf08fc99f68db79
Shawn Carnegie <carnegieshawn@yahoo.ca>
2017-07-19 17:54:16 -0600

    Change user_roles to symbols
    Removed traits in inventory_user factory as user_role can be added with symbol instead of integer
    Added user_role tests checking for validity, default of read, and values within enumerable range

--- spec/factories/inventory_users.rb
+++ spec/factories/inventory_users.rb
@@ -2,17 +2,5 @@ FactoryGirl.define do
   factory :inventory_user do
     association :user, factory: :user
     association :inventory, factory: :inventory
-
-    trait :read do
-      user_role 0
-    end
-
-    trait :write do
-      user_role 1
-    end
-
-    trait :admin do
-      user_role 2
-    end
   end
 end
